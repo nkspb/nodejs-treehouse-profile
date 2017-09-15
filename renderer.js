cee4c096cf129c592072bc6180f0033c474192f3
Nikolay Komolov <nkspb13@gmail.com>
2017-09-15 11:02:05 +0300

    Merged values from JSON to profile template

--- renderer.js
+++ renderer.js
@@ -3,13 +3,23 @@
         // Merge values in to string
 var fs = require('fs');
 
+function mergeValues(values, content) {
+    // cycle over values
+    for (var key in values) {
+    // replace {{key}} with the value from values
+        content = content.replace("{{" + key + "}}", values[key]);
+    }
+    // return merged content
+    return content;
+}
+
 function view(templateName, values, res) {
     // read from template file
-    var data = fs.readFileSync('./views/' + templateName + '.html');
-        // insert values into template
-
-        // write out to the response
-        res.write(data);
+    var data = fs.readFileSync('./views/' + templateName + '.html', {encoding: "utf8"});
+    // insert values into template
+    data = mergeValues(values, data);
+    // write out to the response
+    res.write(data);
 }
 
 module.exports.view = view;
